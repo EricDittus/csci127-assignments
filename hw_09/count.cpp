280f0c4d213ffe0c378e770965078aef2234239d
Eric Dittus <ericdittus@Erics-MacBook-Air.local>
2018-12-02 17:25:42 -0500

    hw

---
+++ hw_09/count.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+  int i = 1;
+  while(i <= 19){
+    std::cout << i << std::endl;
+    i = i + 1;
+  }
+  return 0;
+}
---
+++ hw_09/guess.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using std::cout;
+using std::endl;
+int main(){
+  int a, b, c, min, max;
+
+  std::cout << "Pick a number from 0 to 99, inclusive.\n";
+  std::cin >> a;
+  min = 0;
+  max = 99;
+
+  b = (max + min/2);
+
+  std::cout << "Is your number " << b << "?" << std::endl;
+    std::cout << "Input:" << std::endl;
+    std::cout << "-1: My number is lower, you're bad at this." << std::endl;
+    std::cout << "1: My number is higher, you're still bad at this." << std::endl;
+    std::cout << "0: You Got It! Wow, that was amazing!" << std::endl;
+    std::cin >> c;
+
+  while(c != 0){
+     /*b = min + (rand() % static_cast<int>(max - min + 1));*/
+
+    if(c = -1){
+      max = b;
+      std::cout << max << std::endl;
+      std::cout << min << std::endl;
+
+    }
+    else{
+      min = b;
+      std::cout << max << std::endl;
+      std::cout << min << std::endl;
+    }
+    b = (max + min)/2;
+    std::cout << "Is your number " << b << "?" << std::endl;
+    std::cout << "Input:" << std::endl;
+    std::cout << "-1: My number is lower, you're bad at this." << std::endl;
+    std::cout << "1: My number is higher, you're still bad at this." << std::endl;
+    std::cout << "0: You Got It! Wow, that was amazing!" << std::endl;
+    std::cin >> c;
+  }
+}
